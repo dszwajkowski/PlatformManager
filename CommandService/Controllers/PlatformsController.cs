9a02c6552d0220be65905cfc5baab19ce072a2f4
Xodvrvp <35074897+Xodvrvp@users.noreply.github.com>
2023-06-01 22:52:18 +0200

    Add platforms controller

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers;
+
+[Route("/api/c/[controller]")]
+[ApiController]
+public class PlatformsController : Controller
+{
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+        Console.WriteLine("Command service: Inbound POST");
+        return Ok("Inbound test of from Platforms Controller");
+    }
+}
