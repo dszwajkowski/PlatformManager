c976cc6ed92520146e650e55b5b4adb85a559cd8
Xodvrvp <35074897+Xodvrvp@users.noreply.github.com>
2023-06-19 22:22:11 +0200

    Add command and platform models

---
+++ CommandService/Models/Command.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandService.Models;
+
+public class Command
+{
+    [Key]
+    [Required]
+    public Guid Id { get; set; }
+    [Required]
+    public string HowTo { get; set; } = null!;
+    [Required]
+    public string CommandLine { get; set; } = null!;
+    [Required]
+    public Guid PlatformId { get; set; }
+    public Platform Platform { get; set; } = null!;
+}
---
+++ CommandService/Models/Platform.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandService.Models;
+
+public class Platform
+{
+    [Key]
+    [Required]
+    public Guid Id { get; set; }
+    [Required]
+    public string Name { get; set; } = null!;
+    public ICollection<Command>? Commands { get; set; }
+}
