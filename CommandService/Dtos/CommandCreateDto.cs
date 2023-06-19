88e2c3e694e4cae104c9f717baf8029bc3d03fcb
Xodvrvp <35074897+Xodvrvp@users.noreply.github.com>
2023-06-19 23:03:37 +0200

    Add dtos for command service

---
+++ CommandService/Dtos/CommandCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandService.Dtos;
+
+public class CommandCreateDto
+{
+    [Required]
+    public Guid PlatformId { get; set; }
+    [Required]
+    public string HowTo { get; set; } = null!;
+    [Required]
+    public string CommandLine { get; set; } = null!;
+}
---
+++ CommandService/Dtos/CommandReadDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CommandService.Dtos;
+
+public class CommandReadDto
+{
+    public Guid Id { get; set; }
+    public string HowTo { get; set; } = null!;
+    public string CommandLine { get; set; } = null!;
+    public Guid PlatformId { get; set; }
+}
